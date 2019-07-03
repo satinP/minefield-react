4c7c020b9a956b7c2a9b4fd7393106d00761faa6
Pedro Satin <pedro5satin@gmail.com>
2019-07-03 19:27:32 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import params from './src/params'
+import Field from './src/components/Field';
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Iniciando o Mines!
+        </Text>
+        <Text style={styles.instructions}>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field/>
+       </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
