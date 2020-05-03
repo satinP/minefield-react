10cab5346ec41afb27d24c1311cf265a677f2c4d
Pedro Satin <pedro5satin@gmail.com>
2020-05-03 16:59:27 -0300

    Adicionando componente Mine e funções.

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field';
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
+import Mine from './src/components/Mine';
 
 export default class App extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  getMinesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+  
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return {
+      board: createMinedBoard(rows, cols, this.getMinesAmount()),
+    }
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.welcome}>
           Iniciando o Mines!
         </Text>
         <Text style={styles.instructions}>
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        <Field opened nearMines={1}/>
-       </View>
+        <View style={styles.board}>
+          <MineField board={this.state.board}/>
+        </View>
+       </SafeAreaView>
     );
   }
 }
@@ -22,14 +49,12 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-end',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA',
+    //margin: 10,
   },
   instructions: {
     textAlign: 'center',
